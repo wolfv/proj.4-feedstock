68d18664c9a1d51ca7e4c47c2fe55e50ab87b7e1
ocefpaf <ocefpaf@gmail.com>
2016-11-16 11:18:00 -0300

    Port env vars

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,3 +5,11 @@
 make
 make check
 make install
+
+ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
+DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
+mkdir -p $ACTIVATE_DIR
+mkdir -p $DEACTIVATE_DIR
+
+cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/proj4-activate.sh
+cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/proj4-deactivate.sh
