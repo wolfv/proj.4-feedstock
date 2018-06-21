90a87033452d2229ffe439317d70a81842e48e8a
Filipe Fernandes <ocefpaf@gmail.com>
2018-06-21 11:06:08 -0300

    update recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-pushd $SRC_DIR/nad
-# FIXME: check the sha256 manually 054e7b63b474926c79f25fbe5cd8760351e8d93e5a167b5473e993c01bb08866
-curl -L -O http://download.osgeo.org/proj/proj-datumgrid-1.7.zip
-unzip -o proj-datumgrid-1.7.zip
-rm -rf proj-datumgrid-1.7.zip
-popd
-
-
-./configure --prefix=$PREFIX --without-jni
+./configure --prefix=$PREFIX --without-jni --host=$HOST
 
 export CFLAGS="-O2 -Wl,-S $CFLAGS"
 
 make -j$CPU_COUNT
-make check -j$CPU_COUNT
+# skip tests on linux32 due to rounding error causing issues
+if [[ ! ${HOST} =~ .*linux.* ]] || [[ ! ${ARCH} == 32 ]]; then
+    make check -j$CPU_COUNT
+fi
 make install -j$CPU_COUNT
 
 ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
