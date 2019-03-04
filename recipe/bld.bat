bb0abe4f112dcb856be15cd2a691c2c6c8168e88
snowman2 <alansnow21@gmail.com>
2019-03-03 22:38:59 -0600

    attempt to copy data over in windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,8 @@ cd build
 cmake -G "%CMAKE_GENERATOR%" .. -DCMAKE_BUILD_TYPE=Release -DBUILD_LIBPROJ_SHARED="ON" -DCMAKE_C_FLAGS="/WX" -DCMAKE_CXX_FLAGS="/WX" -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"
 cmake --build . --config Release --target install
 cd ..
+copy /Y data\* %LIBRARY_PREFIX%\\share\\proj
+
 
 if errorlevel 1 exit 1
 
