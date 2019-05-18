f0e1d65dc4b11cf258e509b76f8dc1330053bf73
Filipe <ocefpaf@gmail.com>
2019-05-18 08:57:09 -0300

    minor clean-ups

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
-./configure --prefix=$PREFIX --without-jni --host=$HOST
 
-export CFLAGS="-O2 -Wl,-S $CFLAGS"
+./configure --prefix=${PREFIX} --without-jni --host=${HOST}
 
-make -j$CPU_COUNT
+export CFLAGS="-O2 -Wl,-S ${CFLAGS}"
+
+make -j${CPU_COUNT}
 # skip tests on linux32 due to rounding error causing issues
 if [[ ! ${HOST} =~ .*linux.* ]] || [[ ! ${ARCH} == 32 ]]; then
-    make check -j$CPU_COUNT
+    make check -j${CPU_COUNT}
 fi
-make install -j$CPU_COUNT
+make install -j${CPU_COUNT}
 
-ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
-DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
-mkdir -p $ACTIVATE_DIR
-mkdir -p $DEACTIVATE_DIR
+ACTIVATE_DIR=${PREFIX}/etc/conda/activate.d
+DEACTIVATE_DIR=${PREFIX}/etc/conda/deactivate.d
+mkdir -p ${ACTIVATE_DIR}
+mkdir -p ${DEACTIVATE_DIR}
 
-cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/proj4-activate.sh
-cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/proj4-deactivate.sh
-cp $RECIPE_DIR/scripts/activate.csh $ACTIVATE_DIR/proj4-activate.csh
-cp $RECIPE_DIR/scripts/deactivate.csh $DEACTIVATE_DIR/proj4-deactivate.csh
-cp $RECIPE_DIR/scripts/activate.fish $ACTIVATE_DIR/proj4-activate.fish
-cp $RECIPE_DIR/scripts/deactivate.fish $DEACTIVATE_DIR/proj4-deactivate.fish
-
-# We can remove this when we start using the new conda-build.
-find $PREFIX -name '*.la' -delete
+cp ${RECIPE_DIR}/scripts/activate.sh ${ACTIVATE_DIR}/proj4-activate.sh
+cp ${RECIPE_DIR}/scripts/deactivate.sh ${DEACTIVATE_DIR}/proj4-deactivate.sh
+cp ${RECIPE_DIR}/scripts/activate.csh ${ACTIVATE_DIR}/proj4-activate.csh
+cp ${RECIPE_DIR}/scripts/deactivate.csh ${DEACTIVATE_DIR}/proj4-deactivate.csh
+cp ${RECIPE_DIR}/scripts/activate.fish ${ACTIVATE_DIR}/proj4-activate.fish
+cp ${RECIPE_DIR}/scripts/deactivate.fish ${DEACTIVATE_DIR}/proj4-deactivate.fish
