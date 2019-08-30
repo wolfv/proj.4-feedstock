cc92e50fcac354cc6030c093e2f99dfd9dceea1a
Xylar Asay-Davis <xylarstorm@gmail.com>
2019-08-30 12:58:10 -0600

    Fix activate script for Windows bash

--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -8,8 +8,10 @@ if [[ -n "$PROJ_LIB" ]]; then
 fi
 
 
+# On Linux PROJ_LIB is in $CONDA_PREFIX/share/proj, but
+# Windows keeps it in $CONDA_PREFIX/Library/share
 if [ -d ${CONDA_PREFIX}/share/proj ]; then
   export PROJ_LIB=${CONDA_PREFIX}/share/proj
-elif [ -d ${CONDA_PREFIX}/Library/share/proj ]; then
-  export PROJ_LIB=${CONDA_PREFIX}/Library/share/proj
+elif [ -d ${CONDA_PREFIX}/Library/share ]; then
+  export PROJ_LIB=${CONDA_PREFIX}/Library/share
 fi
