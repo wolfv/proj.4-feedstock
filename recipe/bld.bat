3d081e993668eedfd651df5d444391ffb461ad05
Filipe Fernandes <ocefpaf@gmail.com>
2020-02-13 13:16:56 -0300

    remove unused code

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -12,13 +12,6 @@ if errorlevel 1 exit 1
 cmake --build . --config Release --target install
 if errorlevel 1 exit 1
 
-:: For some reason proj now creates a proj_X_Y.lib so we have to update 
-:: each recipe each time there is a version change. Copy the lib to proj.lib
-FOR /F "tokens=1,2 delims=." %%a IN ("%PKG_VERSION%") DO (
-  set PROJ_MAJOR_VERSION=%%a
-  set PROJ_MINOR_VERSION=%%b
-)
-
 cd ..
 copy /Y data\* %LIBRARY_PREFIX%\\share\\proj
 if errorlevel 1 exit 1
