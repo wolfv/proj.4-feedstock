c42f0f273c7e6a1528bfe626bbb2872b4d7b4cae
Stefan Pfenninger <stefan@pfenninger.org>
2020-04-08 14:10:38 +0200

    Fix fish-incompatible syntax in activate.fish

--- recipe/scripts/activate.fish
+++ recipe/scripts/activate.fish
@@ -10,9 +10,9 @@ else if test -d "$CONDA_PREFIX/Library/share/proj"
   set -gx PROJ_LIB "$CONDA_PREFIX/Library/share/proj"
 end
 
-if test -f "${CONDA_PREFIX}/share/proj/copyright_and_licenses.csv"
+if test -f "$CONDA_PREFIX/share/proj/copyright_and_licenses.csv"
   # proj-data is installed because its license was copied over
-  set -gx PROJ_NETWORK="OFF"
+  set -gx PROJ_NETWORK "OFF"
 else
-  set -gx PROJ_NETWORK="ON"
+  set -gx PROJ_NETWORK "ON"
 end
